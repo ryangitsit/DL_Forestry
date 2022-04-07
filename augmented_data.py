d10088a72647a046ff3d3c697fc2c4149b03556e
EllaKCollins <e.k.collins@student.rug.nl>
2022-04-07 22:17:18 +0200

    augment_data

---
+++ augmented_data.py
@@ -0,0 +1,21 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+import pandas as pd
+
+
+def load_data():
+    aug = ImageDataGenerator(
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        rotation_range=20,
+        zoom_range=0.15,
+        shear_range=0.15,
+        horizontal_flip=True,
+        fill_mode="nearest",
+        validation_split=0.2
+    )
+
+    train_ds = aug.flow_from_directory('forest_data/Data/Train_Data/', target_size=(256, 256), batch_size=32, subset='training', class_mode='binary')
+    val_ds = aug.flow_from_directory('forest_data/Data/Train_Data/', target_size=(256, 256), batch_size=32, subset='validation', class_mode='binary')
+
+    return train_ds, val_ds
