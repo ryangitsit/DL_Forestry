d8abd56df50543d11e296d62ecae5b53d4383a58
Ryan Oloughlin <rmoloughlin11@gmail.com>
2022-04-08 19:21:23 +0200

    Sweep ready!!

--- sweep.bat
+++ sweep.bat
@@ -1,5 +1,5 @@
 ECHO OFF
 
-FOR %%z IN (SGD,RMS,adam) DO FOR %%y IN (.001, .01, .1) DO FOR %%x IN (0, .1, 1) DO FOR %%a IN (True, False) DO py -3.8 ./main.py --epochs 2 --optimizer %%z  --learningrate %%y --momentum %%x --augmentation %%a
+FOR %%z IN (SGD,RMS,adam) DO FOR %%y IN (.001, .01, .1) DO FOR %%x IN (0, .1, 1) DO FOR %%a IN (True, False) DO py -3.8 ./main.py --epochs 3 --optimizer %%z  --learningrate %%y --momentum %%x --augmentation %%a
 
 PAUSE
